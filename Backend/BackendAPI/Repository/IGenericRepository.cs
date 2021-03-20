5f5f323da51c2af134edeb7ce4df4298df4d9f20
Mateusz Sterczewski <mateuszsterczewski@gmail.com>
2021-03-20 11:11:56 +0100

    Dodanie interfejsu IGenericRepository

---
+++ Backend/BackendAPI/Repository/IGenericRepository.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BackendAPI.Repository
+{
+    public interface IGenericRepository<T>//TODO: do rozważenia where T is CoreComponent
+    {
+        T Update(T component);
+        T GetByID(string ID);
+        bool Insert(T component);
+        bool Delete(string ID);
+        void SaveChanges();
+    }
+}
