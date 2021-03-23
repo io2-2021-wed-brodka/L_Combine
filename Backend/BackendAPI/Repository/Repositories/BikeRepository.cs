51984762040a93fd9424268d41494c5eb2d97256
smogork <krzysztof.smogor@wp.pl>
2021-03-23 18:43:35 +0100

    Dodanie klas z repozytorium rowerowego.

---
+++ Backend/BackendAPI/Repository/Repositories/BikeRepository.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using BackendAPI.Models;
+using BackendAPI.Repository.Interfaces;
+
+namespace BackendAPI.Repository.Repositories
+{
+    public class BikeRepository : GenericRepository<Bike>, IBikeRepository
+    {
+        public new bool Delete(string ID)
+        {
+            throw new NotImplementedException();
+        }
+
+        public new IList<Bike> Get()
+        {
+            throw new NotImplementedException();
+        }
+
+        public new Bike GetByID(string ID)
+        {
+            throw new NotImplementedException();
+        }
+
+        public new bool Insert(Bike component)
+        {
+            throw new NotImplementedException();
+        }
+
+        public new void SaveChanges()
+        {
+            throw new NotImplementedException();
+        }
+
+        public new Bike Update(Bike component)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
