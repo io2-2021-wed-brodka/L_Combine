2b7a6a8068c6e5c3a0837ea234ef0865ba796a0d
widomskip <molompl@gmail.com>
2021-04-03 21:31:05 +0200

    Wyrzucenie header-free-requests do osobnego pliku

--- Frontend/UserTech/src/app/interceptors/token.interceptor.ts
+++ Frontend/UserTech/src/app/interceptors/token.interceptor.ts
@@ -1,33 +1,24 @@
-import { Injectable } from '@angular/core';
-import {
-  HttpRequest,
-  HttpHandler,
-  HttpEvent,
-  HttpInterceptor
-} from '@angular/common/http';
-import { Observable } from 'rxjs';
-import { LoginService } from '../services/login.service';
-
-const headerFreeRequests = [
-  {
-    urlTest: /\/api\/login$/,
-    method: 'POST'
-  }
-];
+import {Injectable} from '@angular/core';
+import {HttpEvent, HttpHandler, HttpInterceptor, HttpRequest} from '@angular/common/http';
+import {Observable} from 'rxjs';
+import {LoginService} from '../services/login.service';
+import {HEADER_FREE_REQUESTS} from '../constants/header-free-requests';
 
 @Injectable()
 export class TokenInterceptor implements HttpInterceptor {
 
-  constructor(private loginService: LoginService) {}
+  constructor(private loginService: LoginService) {
+  }
 
   intercept(request: HttpRequest<unknown>, next: HttpHandler): Observable<HttpEvent<unknown>> {
-    if(this.loginService.isLoggedIn() && 
-      !headerFreeRequests.find(tokenFreeReq=> tokenFreeReq.urlTest.test(request.url) && request.method==tokenFreeReq.method)){
-
+    if (this.loginService.isLoggedIn() && !HEADER_FREE_REQUESTS.find(
+        tokenFreeReq => tokenFreeReq.urlTest.test(request.url) &&
+          request.method === tokenFreeReq.method
+      )) {
       console.log('add token to ', request);
       request = request.clone({
         headers: this.loginService.setAuthenticateHeader(request.headers)
-      })
+      });
     }
     return next.handle(request);
   }
