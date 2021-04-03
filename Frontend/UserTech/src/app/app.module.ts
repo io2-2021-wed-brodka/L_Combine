42122601391d05ae0ef1be28ab4ff913ed691cfb
widomskip <molompl@gmail.com>
2021-04-03 23:07:55 +0200

    created basic http-error.interceptor
    (cherry picked from commit 3f3cbd6708286658042631a4a5d863f669e36774)

--- Frontend/UserTech/src/app/app.module.ts
+++ Frontend/UserTech/src/app/app.module.ts
@@ -1,18 +1,19 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
-import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
+import {BrowserModule} from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
+import {HttpClientModule, HTTP_INTERCEPTORS} from '@angular/common/http';
 
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
-import { LoginViewComponent } from './login-view/login-view.component';
-import { FormsModule } from '@angular/forms';
-import { ListStationBikesComponent } from './list-station-bikes/list-station-bikes.component';
-import { ListStationsComponent } from './list-stations/list-stations.component';
-import { ListRentedBikesComponent } from './list-rented-bikes/list-rented-bikes.component';
-import { MainComponent } from './main/main.component';
-import { RentBikeComponent } from './rent-bike/rent-bike.component';
-import { TokenInterceptor } from './interceptors/token.interceptor';
-import { HomeComponent } from './components/home/home.component';
+import {AppRoutingModule} from './app-routing.module';
+import {AppComponent} from './app.component';
+import {LoginViewComponent} from './components/login-view/login-view.component';
+import {FormsModule} from '@angular/forms';
+import {ListStationBikesComponent} from './components/list-station-bikes/list-station-bikes.component';
+import {ListStationsComponent} from './components/list-stations/list-stations.component';
+import {ListRentedBikesComponent} from './components/list-rented-bikes/list-rented-bikes.component';
+import {MainComponent} from './components/main/main.component';
+import {RentBikeComponent} from './components/rent-bike/rent-bike.component';
+import {TokenInterceptor} from './interceptors/token.interceptor';
+import {HttpErrorInterceptor} from './interceptors/http-error.interceptor';
+import {HomeComponent} from './components/home/home.component';
 
 @NgModule({
   declarations: [
@@ -36,8 +37,14 @@ import { HomeComponent } from './components/home/home.component';
       provide: HTTP_INTERCEPTORS,
       useClass: TokenInterceptor,
       multi: true
+    },
+    {
+      provide: HTTP_INTERCEPTORS,
+      useClass: HttpErrorInterceptor,
+      multi: true
     }
   ],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
---
+++ Frontend/UserTech/src/app/interceptors/http-error.interceptor.ts
@@ -0,0 +1,29 @@
+import {Injectable} from '@angular/core';
+import {HttpErrorResponse, HttpEvent, HttpHandler, HttpInterceptor, HttpRequest} from '@angular/common/http';
+import {Observable, throwError} from 'rxjs';
+import {catchError} from 'rxjs/operators';
+
+@Injectable()
+export class HttpErrorInterceptor implements HttpInterceptor {
+
+  constructor() {
+  }
+
+  intercept(request: HttpRequest<unknown>, next: HttpHandler): Observable<HttpEvent<unknown>> {
+    return next.handle(request)
+      .pipe(
+        catchError((error: HttpErrorResponse) => {
+          let errorMessage;
+          if (error.error instanceof ErrorEvent) {
+            // client-side error
+            errorMessage = `Error: ${error.error.message}`;
+          } else {
+            // server-side error
+            errorMessage = `Error Code: ${error.status}\nMessage: ${error.message}`;
+          }
+          window.alert(errorMessage);
+          return throwError(errorMessage);
+        })
+      );
+  }
+}
