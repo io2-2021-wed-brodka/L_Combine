9234f96c5a2408f755996bc41b2d091ff75b9267
KacperWalasek <41973501+KacperWalasek@users.noreply.github.com>
2021-04-13 10:53:47 +0200

    wypożyczanie i oddawanie rowerów

--- Frontend/UserTech/src/app/services/rent-bike.service.ts
+++ Frontend/UserTech/src/app/services/rent-bike.service.ts
@@ -2,19 +2,33 @@ import {Injectable} from '@angular/core';
 import {Bike} from '../models/bike';
 import {Observable, of} from 'rxjs';
 import {RentResult} from '../models/rentResult';
-
+import {environment as env} from '../../environments/environment';
+import {HttpClient} from '@angular/common/http';
+import {map} from 'rxjs/operators';
 @Injectable({
   providedIn: 'root'
 })
 export class RentBikeService {
+  private bikeUrl = `${env.apiUrl}/bikes`;
+  private stationBikeUrl = `${env.apiUrl}/stations/bikes`;
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
 
   rentBike(bike: Bike): Observable<RentResult>{
-    return of({result: 'Ok'});
+    const rentRequest = {
+      id: bike.id
+    };
+    return this.http.post<RentResult>(`${this.bikeUrl}/rented`, rentRequest).pipe(
+      map(()=>({result: 'Ok'}))
+    );
   }
 
-  returnBike(bikeId: string, stationId: string): Observable<any> {
-    return of('a');
+  returnBike(bikeId: string, stationId: string): Observable<boolean> {
+    const returnRequest = {
+      id: bikeId
+    };
+    return this.http.post<RentResult>(`${this.stationBikeUrl}/${stationId}`, returnRequest).pipe(
+      map(()=>true)
+    );
   }
 }
