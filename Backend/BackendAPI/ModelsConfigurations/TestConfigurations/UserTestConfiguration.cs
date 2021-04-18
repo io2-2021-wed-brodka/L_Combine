b9e2995cb1fd9943520a807d48c0992044f049fc
smogor <krzysztof.smogor@wp.pl>
2021-04-18 12:21:26 +0200

    Poprawa struktury folderów

--- Backend/BackendAPI/ModelsConfigurations/TestConfigurations/UserTestConfiguration.cs
+++ Backend/BackendAPI/ModelsConfigurations/TestConfigurations/UserTestConfiguration.cs
@@ -24,7 +24,7 @@ namespace BackendAPI.ModelsConfigurations.TestConfigurations
                 builder.HasData(
                 new User { ID = 1, Name = "ImieTestowe", LastName = "NazwiskoTestowe", Login = "login1", PasswordHash = stringHash.GetHash("pass1") },
                 new User { ID = 2, Name = "Imie2", LastName = "Nazwisko2", Login = "login2", PasswordHash = stringHash.GetHash("pass2") },
-                new User { ID = 3, Name = "Grzegorz", LastName = "Brzęczeszykiewicz", Login = "login3", PasswordHash = stringHash.GetHash("pass3") },
+                new User { ID = 3, Name = "Grzegorz", LastName = "Floryda", Login = "wdech", PasswordHash = stringHash.GetHash("wydech") },
                 new User { ID = 4, Name = "Imie3", LastName = "Nazwisko3", Login = "login4", PasswordHash = stringHash.GetHash("pass4") },
                 new User { ID = 5, Name = "PostmanUserName", LastName = "PostmanUserLastName", Login = "PostmanUser", PasswordHash = stringHash.GetHash("PostmanUserPass") }
                 );
