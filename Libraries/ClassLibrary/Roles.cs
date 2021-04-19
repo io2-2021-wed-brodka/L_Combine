7af1854d28b97111cc3adf93d483d42be52da546
Paweł <01142223@pw.edu.pl>
2021-04-19 20:38:26 +0200

    dodanie klasy z rolami

---
+++ Libraries/ClassLibrary/Roles.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ClassLibrary
+{
+    public static class Roles
+    {
+        public const string User = "User";
+        public const string Tech = "Tech";
+        public const string Admin = "Admin";
+    }
+}
