536e274c3fd77cddb25c0569925c66bec77396b2
smogor <krzysztof.smogor@wp.pl>
2021-04-19 16:39:29 +0200

    Dodanie endpointu do rejestracji.

---
+++ Backend/BackendAPI/Controllers/RegisterController.cs
@@ -0,0 +1,50 @@
+﻿using BackendAPI.Models;
+using BackendAPI.Repository.Interfaces;
+using ClassLibrary;
+using ClassLibrary.Exceptions;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BackendAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class RegisterController : ControllerBase
+    {
+        private IUserRepository userRepository;
+        public RegisterController(IUserRepository userRepository)
+        {
+            this.userRepository = userRepository;
+        }
+
+        //POST api/register
+        [HttpPost]
+        public IActionResult Post([FromBody] AuthenticateRequest model)
+        {
+            //Sprawdz, czy podany login istnieje juz w bazie
+            if (userRepository.Get().Where(u => u.Login == model.Login).Any())
+                throw new HttpResponseException("Given login already exists in database.", 409);
+
+            //Dodaj uzytkownika do bazy
+            using (StringHash stringHash = new StringHash())
+            {
+                userRepository.Insert(new User()
+                {
+                    Login = model.Login,
+                    PasswordHash = stringHash.GetHash(model.Password),
+                    Name = "ROZSZEZRENIE_ENDPOINTU_POTRZEBNE",
+                    LastName = "ROZSZEZRENIE_ENDPOINTU_POTRZEBNE"
+                });
+            }
+            userRepository.SaveChanges();
+
+            //Zwroc token
+            AuthenticateResponse response = userRepository.Authenticate(model);
+
+            return Ok(response);
+        }
+    }
+}
