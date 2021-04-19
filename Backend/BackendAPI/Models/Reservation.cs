5584d5121f07bc2281aca09a3ff915b5bffc2209
smogor <krzysztof.smogor@wp.pl>
2021-04-19 21:21:50 +0200

    Dodanie obiektu modelowego rezerwacji

---
+++ Backend/BackendAPI/Models/Reservation.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BackendAPI.Models
+{
+    public class Reservation
+    {
+        //Klucz główny
+        public int ID { get; set; }
+        public DateTime ReservationDate { get; set; }
+        public DateTime ExpireDate { get; set; }
+
+        public int UserId { get; set; }
+        public User User { get; set; }
+
+        public int BikeId { get; set; }
+        public Bike Bike { get; set; }
+    }
+}
