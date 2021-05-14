9b4dd51e4788eefaaee169e5e5749cbbff695d4f
Paweł <01142223@pw.edu.pl>
2021-05-14 21:58:55 +0200

    400 Bad Request przy złych body requestach

--- Libraries/ClassLibrary/DTO/NewTechDTO.cs
+++ Libraries/ClassLibrary/DTO/NewTechDTO.cs
@@ -1,12 +1,15 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Text;
 
 namespace ClassLibrary.DTO
 {
     public class NewTechDTO
     {
+        [Required]
         public string Name { get; set; }
+        [Required]
         public string Password { get; set; }
     }
 }
