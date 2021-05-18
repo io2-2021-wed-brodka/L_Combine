c5dd5daff760d6f7b1da1e81a8df54d18fad3015
smogork <krzysztof.smogor@wp.pl>
2021-05-19 00:52:40 +0200

    Dodanie wstepnych kontenerw dockerowych

---
+++ scripts/run_test.sh
@@ -0,0 +1,28 @@
+#! /bin/bash
+
+TEST_PATH=$1
+PROJECT_PATH=$2
+
+#Fukcja czyści baze i ustawia ponownie dla wybranego kontekstu
+function clean_database () {
+    dotnet ef database update 0 --no-build --context --project ${PROJECT_PATH} BackendAPI.Data.DataContext
+    dotnet ef database update --no-build --context --project ${PROJECT_PATH} BackendAPI.Data.DataContext
+}
+
+function run_test () {
+    newman run $1 --insecure
+    return $?
+}
+
+for fun in `ls ${TEST_PATH}`
+do
+    echo "Testing ${fun}" 
+    clean_database
+    run_test "${TEST_PATH}/${fun}"
+
+    #W przypadku nieudanego testu, zwroc 1
+    if [[ $? -ne 0 ]]
+    then
+        exit 1
+    fi
+done
