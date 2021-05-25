e5da95b1eea86c6be1447f375e34cae297cf523b
Paweł <01142223@pw.edu.pl>
2021-05-25 20:02:39 +0200

    odutudy

--- Backend/BackendAPI/Program.cs
+++ Backend/BackendAPI/Program.cs
@@ -42,10 +42,6 @@ namespace BackendAPI
 
         public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
             WebHost.CreateDefaultBuilder(args)
-                .ConfigureAppConfiguration((hostingContext, config) => {
-                    config.AddJsonFile("appsettings.Docker.json", optional: false, reloadOnChange: false)
-                        .AddJsonFile("appsettings.Linux.json", optional: false, reloadOnChange: false);
-                })
                 .UseStartup<Startup>();
     }
 }
