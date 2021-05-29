154b4d3d81e14c656d5769ff560866b8f7eb15d0
Paweł <01142223@pw.edu.pl>
2021-05-29 23:02:01 +0200

    Development dla windy, Production dla linuxa i dockera

--- Backend/init.sh
+++ Backend/init.sh
@@ -3,6 +3,7 @@
 set -e
 
 cd BackendAPI
+export ASPNETCORE_ENVIRONMENT=Production
 until dotnet ef database update --no-build --context BackendAPI.Data.DataContext; do
 >&2 echo "SQL Server is starting up"
 sleep 3
