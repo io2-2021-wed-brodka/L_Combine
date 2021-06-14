b8e7e128535a5cb7abf59a8a5c40792cbd1c456b
KacperWalasek <41973501+KacperWalasek@users.noreply.github.com>
2021-06-14 19:34:35 +0200

    przyciski i poprawka funkcji blokujących

--- Frontend/UserTech/src/app/components/malfunctions/malfunction-details/malfunction-details.component.spec.ts
+++ Frontend/UserTech/src/app/components/malfunctions/malfunction-details/malfunction-details.component.spec.ts
@@ -1,17 +1,34 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { of } from 'rxjs';
 import { MalfunctionState } from 'src/app/models/malfunction';
+import { BikeService } from 'src/app/services/bike.service';
+import { MalfunctionService } from 'src/app/services/malfunction.service';
 
 import { MalfunctionDetailsComponent } from './malfunction-details.component';
 
 describe('MalfunctionDetailsComponent', () => {
   let component: MalfunctionDetailsComponent;
   let fixture: ComponentFixture<MalfunctionDetailsComponent>;
+  let bikeService: jasmine.SpyObj<BikeService>;
+  let malfunctionService: jasmine.SpyObj<MalfunctionService>;
 
   beforeEach(async () => {
+    const malfunctionServiceSpy = jasmine.createSpyObj('MalfunctionService', ['deleteMalfunction']);
+    const bikeServiceSpy = jasmine.createSpyObj('BikeService', ['block']);
     await TestBed.configureTestingModule({
-      declarations: [ MalfunctionDetailsComponent ]
+      declarations: [ MalfunctionDetailsComponent ],
+      providers: [
+        {provide: MalfunctionService, useValue: malfunctionServiceSpy},
+        {provide: BikeService, useValue: bikeServiceSpy},
+      ]
     })
     .compileComponents();
+
+    malfunctionService = TestBed.inject(MalfunctionService) as jasmine.SpyObj<MalfunctionService>;
+    bikeService = TestBed.inject(BikeService) as jasmine.SpyObj<BikeService>;
+
+    malfunctionService.deleteMalfunction.and.returnValue(of());
+
   });
 
   beforeEach(() => {
--- Frontend/UserTech/src/app/components/malfunctions/malfunction-details/malfunction-details.component.ts
+++ Frontend/UserTech/src/app/components/malfunctions/malfunction-details/malfunction-details.component.ts
@@ -1,5 +1,7 @@
 import { Component, Input, OnInit, Output, EventEmitter } from '@angular/core';
-import { Malfunction } from 'src/app/models/malfunction';
+import { Malfunction, MalfunctionState } from 'src/app/models/malfunction';
+import { BikeService } from 'src/app/services/bike.service';
+import { MalfunctionService } from 'src/app/services/malfunction.service';
 
 @Component({
   selector: 'app-malfunction-details',
@@ -8,9 +10,31 @@ import { Malfunction } from 'src/app/models/malfunction';
 })
 export class MalfunctionDetailsComponent implements OnInit {
   @Input() malfunction!: Malfunction;
-  constructor() { }
+  @Output() malfunctionChanged = new EventEmitter();
+  constructor(private malfunctionService: MalfunctionService, private bikeService: BikeService) { }
 
   ngOnInit(): void {
   }
-
+  isInReparation(){
+    console.log(this.malfunction.state)
+    return this.malfunction.state === MalfunctionState.InReparation;
+  }
+  isRented(){
+    console.log(this.malfunction.state)
+    return this.malfunction.state === MalfunctionState.BikeRented;
+  }
+  isWaiting(){
+    console.log(this.malfunction.state)
+    return this.malfunction.state === MalfunctionState.Waiting;
+  }
+  deleteMalfunction(){
+    this.malfunctionService.deleteMalfunction(this.malfunction).subscribe(()=>
+      this.malfunctionChanged.emit()
+    );
+    
+  }
+  acceptMalfunction(){
+    //this.bikeService.block()
+    this.malfunctionChanged.emit();
+  }
 }
