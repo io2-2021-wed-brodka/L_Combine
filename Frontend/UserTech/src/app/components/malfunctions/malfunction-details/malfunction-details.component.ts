cec691a2e97427e4d84988e9c5715337cfb1898b
KacperWalasek <41973501+KacperWalasek@users.noreply.github.com>
2021-06-14 20:28:25 +0200

    komunikaty

--- Frontend/UserTech/src/app/components/malfunctions/malfunction-details/malfunction-details.component.ts
+++ Frontend/UserTech/src/app/components/malfunctions/malfunction-details/malfunction-details.component.ts
@@ -3,6 +3,7 @@ import { forkJoin } from 'rxjs';
 import { Malfunction, MalfunctionState } from 'src/app/models/malfunction';
 import { BikeService } from 'src/app/services/bike.service';
 import { MalfunctionService } from 'src/app/services/malfunction.service';
+import { NotificationService } from 'src/app/services/notification.service';
 
 @Component({
   selector: 'app-malfunction-details',
@@ -12,7 +13,11 @@ import { MalfunctionService } from 'src/app/services/malfunction.service';
 export class MalfunctionDetailsComponent implements OnInit {
   @Input() malfunction!: Malfunction;
   @Output() malfunctionChanged = new EventEmitter();
-  constructor(private malfunctionService: MalfunctionService, private bikeService: BikeService) { }
+  constructor(
+    private malfunctionService: MalfunctionService, 
+    private bikeService: BikeService,
+    private notificationService: NotificationService
+  ) { }
 
   ngOnInit(): void {
   }
@@ -26,22 +31,25 @@ export class MalfunctionDetailsComponent implements OnInit {
     return this.malfunction.state === MalfunctionState.Waiting;
   }
   cancelMalfunction(){
-    this.malfunctionService.deleteMalfunction(this.malfunction).subscribe(()=>
-      this.malfunctionChanged.emit()
-    );
+    this.malfunctionService.deleteMalfunction(this.malfunction).subscribe(()=>{
+      this.malfunctionChanged.emit();
+      this.notificationService.success("Usterka została anulowana");
+    });
     
   }
   acceptMalfunction(){
-    this.bikeService.block(this.malfunction.bikeId).subscribe(()=>
-      this.malfunctionChanged.emit()
-    );
+    this.bikeService.block(this.malfunction.bikeId).subscribe(()=>{
+      this.malfunctionChanged.emit();
+      this.notificationService.success("Rower został zablokowany do naprawy");
+    });
   }
   repereBike(){
     forkJoin([
       this.bikeService.unblock(this.malfunction.bikeId),
       this.malfunctionService.deleteMalfunction(this.malfunction)
-    ]).subscribe(()=>
-      this.malfunctionChanged.emit()
-    );
+    ]).subscribe(()=>{
+      this.malfunctionChanged.emit();
+      this.notificationService.success("Usterka została naprawiona");
+    });
   }
 }
