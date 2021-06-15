97e1c3f6e5b69a0258dce54d3106fc151dbdf6ea
KacperWalasek <41973501+KacperWalasek@users.noreply.github.com>
2021-06-15 14:51:59 +0200

    testy sprintu 5

--- Frontend/UserTech/e2e/src/pages/malfunctions.po.ts
+++ Frontend/UserTech/e2e/src/pages/malfunctions.po.ts
@@ -1,4 +1,4 @@
-import {$, browser, ElementArrayFinder, promise} from 'protractor';
+import {$, browser, ElementArrayFinder, ElementFinder, promise} from 'protractor';
 
 export class MalfunctionsPage {
   navigateToMalfunctions(): promise.Promise<any> {
@@ -7,5 +7,21 @@ export class MalfunctionsPage {
 
   getMalfunctions(): ElementArrayFinder {
     return $('app-list-malfunctions').$$('.list-item');
+  }
+
+  getMalfunctionCancelButton(malfunction: ElementFinder): ElementFinder {
+    return malfunction.$$('button').first();
+  }
+
+  getMalfunctionAcceptButton(malfunction: ElementFinder): ElementFinder {
+    return malfunction.$$('button').last();
+  }
+
+  getMalfunctionRepairButton(malfunction: ElementFinder): ElementFinder {
+    return malfunction.$('button');
+  }
+
+  getMalfunctionButtons(malfunction: ElementFinder): ElementArrayFinder {
+    return malfunction.$$('button');
   }
 }
